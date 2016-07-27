a7cd14345c18ef9192af29792e65e65b609782bf
Simonas Kazlauskas <git@kazlauskas.me>
2016-07-27 20:14:06 +0300

    Script to generate docs

---
+++ make-doc.sh
@@ -0,0 +1,7 @@
+rm -rf doc/* target/*
+cargo doc --target=x86_64-unknown-linux-gnu
+cargo doc --target=x86_64-pc-windows-msvc
+cp -rf target/x86_64-unknown-linux-gnu/doc/* doc/
+cp -rf target/x86_64-pc-windows-msvc/doc/libloading/os/windows/* ./doc/libloading/os/windows/
+cd doc
+git checkout index.html
