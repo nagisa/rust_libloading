945e7374baf56ecbea04d6d039326912d3407925
Simonas Kazlauskas <git@kazlauskas.me>
2016-07-28 16:36:48 +0300

    Do not ignore build failures in doc script

--- make-doc.sh
+++ make-doc.sh
@@ -1,7 +1,8 @@
-rm -rf doc/* target/*
-cargo doc --target=x86_64-unknown-linux-gnu
-cargo doc --target=x86_64-pc-windows-msvc
-cp -rf target/x86_64-unknown-linux-gnu/doc/* doc/
-cp -rf target/x86_64-pc-windows-msvc/doc/libloading/os/windows/* ./doc/libloading/os/windows/
-cd doc
+rm -rf doc/* target/* &&
+cargo doc --target=x86_64-unknown-linux-gnu &&
+cargo doc --target=x86_64-pc-windows-msvc &&
+cp -rf target/x86_64-unknown-linux-gnu/doc/* doc/ &&
+cp -rf target/x86_64-pc-windows-msvc/doc/libloading/os/windows/* ./doc/libloading/os/windows/ &&
+cp -rf target/x86_64-pc-windows-msvc/doc/{winapi,kernel32} ./doc/ &&
+cd doc &&
 git checkout index.html
