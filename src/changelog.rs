ca73d50a351c34d0198158ee4466d3931d4374fd
Simonas Kazlauskas <git@kazlauskas.me>
2017-12-07 21:29:49 +0200

    Release 0.4.3

--- src/changelog.rs
+++ src/changelog.rs
@@ -1,11 +1,17 @@
 //! Project changelog
 
+/// Release 0.4.3 (2017-12-07)
+///
+/// * Bump lazy-static dependency to `^1.0`
+/// * `cargo test --release` now works when testing libloading.
+pub mod r0_4_3 {}
+
 
 /// Release 0.4.2 (2017-09-24)
 ///
 /// * Improved error and race-condition handling on Windows;
 /// * Improved documentation about thread-safety of Library;
-/// * Added Symbol::<Option<T>::lift_option() -> Option<Symbol<T>> convenience method.
+/// * Added `Symbol::<Option<T>::lift_option() -> Option<Symbol<T>>` convenience method.
 pub mod r0_4_2 {}
 
 
