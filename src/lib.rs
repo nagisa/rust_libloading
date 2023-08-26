9c732cd4000bb6bd71b0e2a8be95549ef36b179e
Snowiiii <71594357+Snowiiii@users.noreply.github.com>
2023-08-26 21:39:32 +0200

    Cargo: fmt

--- src/lib.rs
+++ src/lib.rs
@@ -35,7 +35,10 @@
 //!
 //! The compiler will ensure that the loaded function will not outlive the `Library` from which it comes,
 //! preventing the most common memory-safety issues.
-#![cfg_attr(any(unix, windows), deny(missing_docs, clippy::all, unreachable_pub, unused))]
+#![cfg_attr(
+    any(unix, windows),
+    deny(missing_docs, clippy::all, unreachable_pub, unused)
+)]
 #![cfg_attr(libloading_docs, feature(doc_cfg))]
 
 pub mod changelog;
